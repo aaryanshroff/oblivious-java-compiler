36d58cef99d1dc1ce4caf0a3ab2679e4d0686754
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2024-06-07 15:15:32 -0400

    Transform and test in one file

--- src/main/java/com/example/MemoryAccessTransformer.java
+++ src/main/java/com/example/MemoryAccessTransformer.java
@@ -1,34 +1,44 @@
 package com.example;
 
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.*;
 
-public class MemoryAccessTransformer extends ClassVisitor implements Opcodes {
-  public MemoryAccessTransformer(ClassVisitor cv) {
-    super(ASM9, cv);
-  }
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-  @Override
-  public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
-    MethodVisitor mv = cv.visitMethod(access, name, descriptor, signature, exceptions);
-    return new MethodVisitor(ASM9, mv) {
+import static org.objectweb.asm.Opcodes.*;
+
+public class MemoryAccessTransformer {
+  public static void main(String[] args) throws IOException {
+    String className = "com/example/App";
+    byte[] classBytes = Files.readAllBytes(Paths.get("target/classes/" + className.replace('.', '/') + ".class"));
+    ClassReader classReader = new ClassReader(classBytes);
+    ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_FRAMES);
+    ClassVisitor classVisitor = new ClassVisitor(ASM9, classWriter) {
       @Override
-      public void visitInsn(int opcode) {
-        switch (opcode) {
-          case IALOAD:
-            // Replace IALOAD with PathORAM.read
-            mv.visitInsn(SWAP); // Swap array reference and index
-            mv.visitMethodInsn(INVOKESTATIC, "com/example/PathORAM", "read", "(I)I", false);
-            break;
-          case IASTORE:
-            // Replace IASTORE with PathORAM.write
-            mv.visitMethodInsn(INVOKESTATIC, "com/example/PathORAM", "write", "(II)V", false);
-            break;
-          default:
-            super.visitInsn(opcode);
-        }
+      public MethodVisitor visitMethod(int access, String name, String descriptor, String signature,
+          String[] exceptions) {
+        MethodVisitor mv = super.visitMethod(access, name, descriptor, signature, exceptions);
+        return new MethodVisitor(ASM9, mv) {
+          @Override
+          public void visitInsn(int opcode) {
+            switch (opcode) {
+              case IALOAD:
+                mv.visitMethodInsn(Opcodes.INVOKESTATIC, "com/example/PathORAM", "read", "([II)I", false);
+                break;
+              default:
+                super.visitInsn(opcode);
+                break;
+            }
+          }
+        };
       }
     };
+    classReader.accept(classVisitor, 0);
+    byte[] transformedClassBytes = classWriter.toByteArray();
+    try (FileOutputStream fos = new FileOutputStream("target/classes/" + className.replace('.', '/') + ".class")) {
+      fos.write(transformedClassBytes);
+    }
   }
 }
