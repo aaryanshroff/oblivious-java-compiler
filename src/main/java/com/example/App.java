2fabd7318925d9d516c032cf381397396e553bbf
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2024-06-07 15:49:34 -0400

    More extensive tests

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,6 +4,10 @@ public class App {
   public static void main(String[] args) {
     int[] array = new int[10];
     array[0] = 1;
+    array[1] = array[0];
+    array[2] = 5;
     System.out.println(array[0]);
+    System.out.println(array[1]);
+    System.out.println(array[2]);
   }
 }
