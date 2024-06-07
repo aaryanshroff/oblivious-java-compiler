b3dad39364474914d893010586ff361e388c1678
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2024-06-07 15:20:57 -0400

    Test array replacement

--- src/main/java/com/example/PathORAM.java
+++ src/main/java/com/example/PathORAM.java
@@ -3,10 +3,11 @@ package com.example;
 public class PathORAM {
     public static int read(int[] arrayref, int index) {
         System.out.println("Path ORAM read at address: " + index);
-        return 0; // placeholder value
+        return arrayref[index];
     }
 
     public static void write(int[] arrayref, int index, int value) {
         System.out.println("Path ORAM write at address: " + index + " with value: " + value);
+        arrayref[index] = value;
     }
 }
