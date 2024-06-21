ccf284acdc83ec579e9e0b7ebb76d6a66775f9e3
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2024-06-21 14:42:05 -0400

    More tests

--- src/test/java/com/example/PathORAMTest.java
+++ src/test/java/com/example/PathORAMTest.java
@@ -26,7 +26,7 @@ public class PathORAMTest {
 
     // Ensure that the data is not null and the correct data was read
     assertTrue(dataRead.isPresent());
-    assertNotNull(dataRead.get());
+    assertArrayEquals(dataToWrite, dataRead.get());
   }
 
   @Test
@@ -46,5 +46,70 @@ public class PathORAMTest {
     // Assert that the data read is the same as the new data written
     assertTrue(readData.isPresent()); // Ensure that the readData is present
     assertArrayEquals(newData, readData.get());
+  }
+
+  @Test
+  public void testReadUninitializedBlock() {
+    String blockId = "uninitializedBlock";
+    Optional<byte[]> dataRead = pathORAM.access(blockId, Optional.empty(), false);
+
+    // Ensure that the data read is empty
+    assertFalse(dataRead.isPresent());
+  }
+
+  @Test
+  public void testWriteNullData() {
+    String blockId = "block3";
+
+    // Attempt to write null data
+    assertThrows(NullPointerException.class, () -> {
+      pathORAM.access(blockId, Optional.of(null), true);
+    });
+  }
+
+  @Test
+  public void testLargeDataBlock() {
+    String blockId = "largeBlock";
+    byte[] largeData = new byte[1024 * 1024]; // 1 MB of data
+    for (int i = 0; i < largeData.length; i++) {
+      largeData[i] = (byte) (i % 256);
+    }
+
+    pathORAM.access(blockId, Optional.of(largeData), true);
+    Optional<byte[]> dataRead = pathORAM.access(blockId, Optional.empty(), false);
+
+    assertTrue(dataRead.isPresent());
+    assertArrayEquals(largeData, dataRead.get());
+  }
+
+  @Test
+  public void testMultipleBlocks() {
+    String blockId1 = "block5";
+    String blockId2 = "block6";
+
+    byte[] data1 = new byte[] { 1, 1, 1, 1 };
+    byte[] data2 = new byte[] { 2, 2, 2, 2 };
+
+    pathORAM.access(blockId1, Optional.of(data1), true);
+    pathORAM.access(blockId2, Optional.of(data2), true);
+
+    Optional<byte[]> dataRead1 = pathORAM.access(blockId1, Optional.empty(), false);
+    Optional<byte[]> dataRead2 = pathORAM.access(blockId2, Optional.empty(), false);
+
+    assertTrue(dataRead1.isPresent());
+    assertArrayEquals(data1, dataRead1.get());
+
+    assertTrue(dataRead2.isPresent());
+    assertArrayEquals(data2, dataRead2.get());
+  }
+
+  @Test
+  public void testInvalidBlockId() {
+    String invalidBlockId = "invalidBlock";
+
+    // Ensure that accessing an invalid block ID doesn't throw an exception
+    assertDoesNotThrow(() -> {
+      pathORAM.access(invalidBlockId, Optional.empty(), false);
+    });
   }
 }
