f74ae422c018512e74129189974cbf30a486686a
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2024-06-21 14:55:36 -0400

    Fix MemoryAccessTransformer

--- src/main/java/com/example/MemoryAccessTransformer.java
+++ src/main/java/com/example/MemoryAccessTransformer.java
@@ -25,25 +25,24 @@ public class MemoryAccessTransformer {
           public void visitInsn(int opcode) {
             switch (opcode) {
               case IALOAD:
-                mv.visitMethodInsn(INVOKESTATIC, "com/example/PathORAM", "readIntArray", "([II)I", false);
+                mv.visitMethodInsn(INVOKESTATIC, "com/example/ORAMAccessHelper", "readIntArray", "([II)I", false);
                 break;
               case IASTORE:
-                mv.visitMethodInsn(INVOKESTATIC, "com/example/PathORAM", "writeIntArray", "([III)V", false);
+                mv.visitMethodInsn(INVOKESTATIC, "com/example/ORAMAccessHelper", "writeIntArray", "([III)V", false);
                 break;
               case FALOAD:
-                mv.visitMethodInsn(INVOKESTATIC, "com/example/PathORAM", "readFloatArray", "([FI)F", false);
+                mv.visitMethodInsn(INVOKESTATIC, "com/example/ORAMAccessHelper", "readFloatArray", "([FI)F", false);
                 break;
               case FASTORE:
-                mv.visitMethodInsn(INVOKESTATIC, "com/example/PathORAM", "writeFloatArray", "([FIF)V", false);
+                mv.visitMethodInsn(INVOKESTATIC, "com/example/ORAMAccessHelper", "writeFloatArray", "([FIF)V", false);
                 break;
-              // Add cases for other types, e.g., LALOAD, LASTORE, etc.
               case AALOAD:
-                mv.visitMethodInsn(INVOKESTATIC, "com/example/PathORAM", "readObjectArray",
+                mv.visitMethodInsn(INVOKESTATIC, "com/example/ORAMAccessHelper", "readObjectArray",
                     "([Ljava/lang/Object;I)Ljava/lang/Object;", false);
                 mv.visitTypeInsn(CHECKCAST, "java/lang/String");
                 break;
               case AASTORE:
-                mv.visitMethodInsn(INVOKESTATIC, "com/example/PathORAM", "writeObjectArray",
+                mv.visitMethodInsn(INVOKESTATIC, "com/example/ORAMAccessHelper", "writeObjectArray",
                     "([Ljava/lang/Object;ILjava/lang/Object;)V", false);
                 break;
               default:
@@ -53,33 +52,76 @@ public class MemoryAccessTransformer {
           }
 
           @Override
-          public void visitIntInsn(int opcode, int operand) {
-            // Handle array initialization opcodes
-            if (opcode == Opcodes.NEWARRAY || opcode == Opcodes.ANEWARRAY) {
-              // Push 0 onto the stack for array size
-              super.visitInsn(Opcodes.ICONST_0);
-              // Call the appropriate array creation instruction
-              super.visitIntInsn(opcode, operand);
-            } else {
-              super.visitIntInsn(opcode, operand);
+          public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
+            switch (opcode) {
+              case GETFIELD:
+                // Push the field name onto the stack
+                super.visitLdcInsn(name);
+                switch (descriptor) {
+                  case "I":
+                  case "B":
+                  case "C":
+                  case "S":
+                  case "Z":
+                    super.visitMethodInsn(INVOKESTATIC, "com/example/ORAMAccessHelper", "readIntField",
+                        "(Ljava/lang/Object;Ljava/lang/String;)I", false);
+                    break;
+                  case "Ljava/lang/String;":
+                    super.visitMethodInsn(INVOKESTATIC, "com/example/ORAMAccessHelper", "readStringField",
+                        "(Ljava/lang/Object;Ljava/lang/String;)Ljava/lang/String;", false);
+                    break;
+                  default:
+                    super.visitFieldInsn(opcode, owner, name, descriptor);
+                }
+                break;
+              case PUTFIELD:
+                // Push the field name onto the stack
+                super.visitLdcInsn(name);
+                // Swap the object reference and field name on the stack
+                super.visitInsn(SWAP);
+                // Switch on the type of the value
+                switch (descriptor) {
+                  case "I":
+                  case "B":
+                  case "C":
+                  case "S":
+                  case "Z":
+                    super.visitMethodInsn(INVOKESTATIC, "com/example/ORAMAccessHelper", "writeIntField",
+                        "(Ljava/lang/Object;Ljava/lang/String;I)V", false);
+                    break;
+                  case "Ljava/lang/String;":
+                    super.visitMethodInsn(INVOKESTATIC, "com/example/ORAMAccessHelper", "writeStringField",
+                        "(Ljava/lang/Object;Ljava/lang/String;Ljava/lang/String;)V", false);
+                    break;
+                  default:
+                    super.visitFieldInsn(opcode, owner, name, descriptor);
+                }
+                break;
+              default:
+                super.visitFieldInsn(opcode, owner, name, descriptor);
             }
           }
-
-          @Override
-          public void visitTypeInsn(int opcode, String type) {
-            if (opcode == Opcodes.ANEWARRAY) {
-              // Push 0 onto the stack for array size
-              super.visitInsn(Opcodes.ICONST_0);
-            }
-            super.visitTypeInsn(opcode, type);
-          }
-
         };
       }
     };
     classReader.accept(classVisitor, 0);
     byte[] transformedClassBytes = classWriter.toByteArray();
     try (FileOutputStream fos = new FileOutputStream("target/classes/" + className.replace('.', '/') + ".class")) {
+      fos.write(transformedClassBytes);
+    }
+  }
+
+  public static void transformClass(String className) throws IOException {
+    String classPath = className.replace('.', '/') + ".class";
+    byte[] classBytes = Files.readAllBytes(Paths.get("target/test-classes/" + classPath));
+    ClassReader classReader = new ClassReader(classBytes);
+    ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_FRAMES);
+    ClassVisitor classVisitor = new ClassVisitor(ASM9, classWriter) {
+      // ... existing visitor code ...
+    };
+    classReader.accept(classVisitor, 0);
+    byte[] transformedClassBytes = classWriter.toByteArray();
+    try (FileOutputStream fos = new FileOutputStream("target/test-classes/" + classPath)) {
       fos.write(transformedClassBytes);
     }
   }
